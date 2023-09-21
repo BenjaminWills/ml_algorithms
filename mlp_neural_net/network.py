8f745abc1aff7694053b85d0e2206ede85829722
Benjaminwills <benjaminwills047@gmail.com>
2023-09-21 21:22:52 +0100

    add weight and bias initialisation

--- mlp_neural_net/network.py
+++ mlp_neural_net/network.py
@@ -11,9 +11,7 @@ class Network:
         self.layers = self.__initialise_layers(layer_depths, activations)
 
         # Initialise weights
-        self.weights = []
-        for index in range(layer_depths - 1):
-            pass
+        self.weights = self.__initialise_weights(layer_depths)
 
     def __initialise_layers(
         self, layer_depths: List[int], activations: List[Callable]
@@ -38,4 +36,29 @@ class Network:
         ]
 
     def __initialise_weights(self, layer_depths: List[int]) -> List[np.array]:
-        pass
+        """Initialises the weights between the layers.
+
+        Parameters
+        ----------
+        layer_depths : List[int]
+            A list of the depths of the layers
+
+        Returns
+        -------
+        List[np.array]
+            A list of weights
+        """
+        layer_weights = []
+        for index in range(layer_depths - 1):
+            """
+            The weights have a dimension of layer_depths[index] x layer_depths[index + 1]
+            and describe how each layer relates to the next.
+            """
+            current_depth = layer_depths[index]
+            next_depth = layer_depths[index + 1]
+
+            weights = np.random.rand(current_depth, next_depth)
+
+            layer_weights.append(weights)
+
+        return layer_weights
