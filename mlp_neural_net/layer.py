c7360b862e02e894aba2c2e9a05f10a5e2d5056b
Benjaminwills <benjaminwills047@gmail.com>
2023-09-24 21:25:23 +0100

    Working bias derivative

--- mlp_neural_net/layer.py
+++ mlp_neural_net/layer.py
@@ -12,7 +12,9 @@ class Layer:
     A layer is a collection of nodes.
     """
 
-    def __init__(self, depth: int, activation: Callable) -> None:
+    def __init__(
+        self, depth: int, activation: Callable, biases: np.array = None
+    ) -> None:
         self.activation = activation
         self.depth = depth
 
@@ -20,8 +22,11 @@ class Layer:
         self.nodes = [Node(bias) for bias in self.biases]
         self.values = [node.value for node in self.nodes]
 
-    def __initialise_biases(self) -> np.array:
-        biases = (
-            np.random.rand(self.depth) * RANDOM_MULITPLIER
-        )  # A depth dimensional vector.
-        return biases
+    def __initialise_biases(self, biases: np.array = None) -> np.array:
+        if biases:
+            return biases
+        else:
+            biases = (
+                np.random.rand(self.depth) * RANDOM_MULITPLIER
+            )  # A depth dimensional vector.
+            return biases
--- mlp_neural_net/network.py
+++ mlp_neural_net/network.py
@@ -9,30 +9,57 @@ import numpy as np
 
 from layer import Layer, RANDOM_MULITPLIER
 
+from copy import deepcopy
 from typing import List, Callable, Union
 from tqdm import tqdm
 
 logger = make_logger(logging_path="./mlp-nn.log", save_logs=True, logger_name="MLPNN")
 
+INCREMENT = 10e-5
+
+Weights = List[np.array]
+Weight_derivative = List[np.array]
+Biases = List[np.array]
+Bias_derivative = List[np.array]
+
+
+def MSE(
+    predictions: np.array,
+    truth: np.array,
+) -> float:
+    num_inputs = len(predictions)
+    return (1 / num_inputs) * (np.linalg.norm(truth - predictions))
+
 
 class Network:
     def __init__(
-        self, layer_depths: List[int], activations: Union[List[Callable], Callable]
+        self,
+        layer_depths: List[int],
+        activations: Union[List[Callable], Callable],
+        weights: List[np.array] = None,
+        biases: List[np.array] = None,
     ) -> None:
         # Initialise layers
-        self.layers = self.__initialise_layers(layer_depths, activations)
+        self.layers = self.__initialise_layers(layer_depths, activations, biases)
 
         # Save number of layers
         self.num_layers = len(layer_depths)
+        self.layer_depths = layer_depths
 
         # Initialise weights
-        self.weights = self.__initialise_weights(layer_depths)
+        self.weights = weights or self.__initialise_weights(layer_depths)
+
+        # Initialise biases
+        self.biases = self.__get_biases()
 
         # Initialise activations
         self.activations = activations
 
     def __initialise_layers(
-        self, layer_depths: List[int], activations: List[Callable]
+        self,
+        layer_depths: List[int],
+        activations: List[Callable],
+        biases: Biases,
     ) -> List[Layer]:
         """Initialises the layers of the network.
 
@@ -48,10 +75,21 @@ class Network:
         List[Layer]
             A list of neural network layers
         """
-        return [
-            Layer(depth, activation_function)
-            for (depth, activation_function) in zip(layer_depths, activations)
-        ]
+        if biases:
+            # If biases is not none
+
+            return [
+                Layer(depth, activation_function, bias)
+                for (depth, activation_function, bias) in zip(
+                    layer_depths, activations, biases
+                )
+            ]
+        else:
+            # If biases is none then we randomly allocate biases
+            return [
+                Layer(depth, activation_function)
+                for (depth, activation_function) in zip(layer_depths, activations)
+            ]
 
     def __initialise_weights(self, layer_depths: List[int]) -> List[np.array]:
         """Initialises the weights between the layers.
@@ -66,7 +104,7 @@ class Network:
         List[np.array]
             A list of weights
         """
-        layer_weights = []
+        layer_weights: Weights = []
         for index in range(self.num_layers - 1):
             """
             The weights have a dimension of layer_depths[index] x layer_depths[index + 1]
@@ -81,6 +119,21 @@ class Network:
 
         return layer_weights
 
+    def __get_biases(self) -> List[np.array]:
+        """Gets the biases from the layers of the network.
+        (Used in the backpropogation step)
+
+        Returns
+        -------
+        List[np.array]
+            List of bias vectors for each layer, the shape
+            need not be uniform - dependent on the layer
+            depth at that point.
+        """
+        layers = self.layers
+        biases: Biases = [layer.biases for layer in layers]
+        return biases
+
     def propogate_forwards(self, entries: np.array) -> None:
         """Propogates an input forwards through a network, this means that the weights are used to
         calculate the value for each node.
@@ -89,7 +142,7 @@ class Network:
         -------
         None
         """
-        for index in tqdm(range(self.num_layers)):
+        for index in range(self.num_layers):
             """
             At each layer we want to calcualte the value for each node.
             """
@@ -122,7 +175,102 @@ class Network:
                 # Update the class variable.
                 self.layers[index] = current_layer
 
-        logger.info("Fed forwards.")
+    def propogate_backwards(self, cost_function: Callable) -> None:
+        """Propogate backwards through the network to update the weights and biases.
 
-    def get_output(self):
+        NOTE: going to brute force this one. Fix with algebra in future.
+
+        Parameters
+        ----------
+        cost_function : Callable
+            We expect our cost function to be a function of the weights, biases and the
+            input of the function. i.e C = C(x, [W_1,...,W_n-1], [b_1,...,b_n]).
+        """
+        pass
+
+    def calculate_bias_derivative(
+        self, entries: np.array, truth: np.array
+    ) -> Bias_derivative:
+        """Calculates partial derivative w.r.t each bias in the network.
+
+        Parameters
+        ----------
+        cost_function : Callable
+            We expect our cost function to be a function of the weights, biases and the
+            input of the function. i.e C = C(x, [W_1,...,W_n-1], [b_1,...,b_n]).
+
+        Returns
+        -------
+        List[np.array]
+            A list of all of the partial derivates in the network biases. This will be
+            in the same order as the normal biases
+        """
+        # Propogate forwards and retrieve entries.
+        self.propogate_forwards(entries)
+        predictions = self.get_output()
+
+        # Calculate oringinal MSE
+        original_mse = MSE(predictions, truth)
+
+        # Extract biases from the network
+        network_biases = self.__get_biases()
+
+        # Initialise bias derivatives
+        bias_derivatives: List[np.array] = []
+
+        for index in tqdm(range(self.num_layers)):
+            biases = self.layers[index].biases
+            dimension = biases.shape[0]
+
+            # Initialise derivative vector
+            layer_bias_derivative = np.zeros(dimension)
+
+            # Get derivative
+            for bias_index in range(dimension):
+
+                # Create a new network with these biases.
+                # We only want to update one element of the biases and keep the rest the same
+                # That is to say that we want to update the index, bias_index'th element
+                incremented_bias = deepcopy(network_biases)
+                incremented_bias[index][bias_index] += INCREMENT
+
+                incremented_network = Network(
+                    self.layer_depths, self.activations, self.weights, incremented_bias
+                )
+
+                # Generate the new predicion
+                incremented_network.propogate_forwards(entries)
+                incremented_prediction = incremented_network.get_output()
+
+                # Calculate new MSE
+                new_mse = MSE(
+                    incremented_prediction,
+                    truth,
+                )
+
+                # Calculate partial derivative
+                partial_derivative = (new_mse - original_mse) / INCREMENT
+
+                # Update layer bias derivative
+                layer_bias_derivative[bias_index] = partial_derivative
+
+                # Remove the copy to save memory
+                del incremented_bias
+
+            bias_derivatives.append(layer_bias_derivative)
+        return bias_derivatives
+
+    def calculate_weight_derivtives(
+        self, entries: np.array, truth: np.array
+    ) -> Weight_derivative:
+        pass
+
+    def get_output(self) -> np.array:
+        """Gets the output of the network.
+
+        Returns
+        -------
+        np.array
+            An array of the outputs of the network
+        """
         return self.layers[-1].values
